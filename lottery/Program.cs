3dbb4095856863ddba5c49dd6a8a99496863405f
Konstantinos Egkarchos <konsnosl@gmail.com>
2017-02-03 09:10:14 +0200

    minor text changes.

--- lottery/Program.cs
+++ lottery/Program.cs
@@ -7,6 +7,25 @@ namespace lottery
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Error....");
+            Console.WriteLine();
+            
+            while (!Console.KeyAvailable)
+                Thread.Sleep(250);
+            Console.ReadKey(true);
+            Console.WriteLine("Error λέμε....");
+            Console.WriteLine();
+
+            while (!Console.KeyAvailable)
+                Thread.Sleep(250);
+            Console.ReadKey(true);
+            Console.WriteLine("Καλά....");
+            Console.WriteLine();
+
+            while (!Console.KeyAvailable)
+                Thread.Sleep(250);
+            Console.ReadKey(true);
+
             Lottery lot = new Lottery();
 
             Console.WriteLine("Press ESC to close");
